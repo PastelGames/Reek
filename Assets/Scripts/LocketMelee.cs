36bde8c11d87a3f96294ac11c8a16181236867d2
timmarr98 <tmarr006@fiu.edu>
2020-06-23 20:23:55 -0400

    Fixing Melee Fighting

---
+++ Assets/Scripts/LocketMelee.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocketMelee : MonoBehaviour
+{
+   private float attackDiff;
+   public float startTimeBtwAttack;
+   public Transform attackPos;
+   public float attackRange;
+   public LayerMask whatIsEnemies;
+   void Update()
+   {
+       if(attackDiff <= 0){
+           if(Input.GetKey(KeyCode.W)){
+               Collider2D [] enemiesDamaged = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+               for (int i =0; i < enemiesDamaged.Length; i++){
+                   enemiesDamaged[i].GetComponent<Enemy>().DamageTaken(damage);
+               }
+
+           }
+           attackDiff = startTimeBtwAttack;
+       }
+
+       else{
+           attackDiff -= Time.deltaTime;
+       }
+   }
+   void OnDrawGizmosSelected(){
+       OnDrawGizmosSelected.color = Color.red;
+       OnDrawGizmosSelected.DrawWireSphere(attackPos.position, attackRange);
+   }
+}
