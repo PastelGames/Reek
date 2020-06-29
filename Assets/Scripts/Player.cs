a7ef9eb14377fcdaec55adb40335434eff7cd251
El'Christopher Tucker <elchristopher@outlook.com>
2020-06-29 18:24:55 -0400

    added third puzzle camera can now toggle between focusing on the player and the cat

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int health; 
+    public int health;
+
+    CameraFollow cf;
+    GameObject cat;
+
+    private void Start()
+    {
+        cf = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>();
+        cat = GameObject.FindGameObjectWithTag("Cat");
+    }
+
+    private void Update()
+    {
+        //if the mouse wheel is scrolled
+        if (Mathf.Abs(Input.mouseScrollDelta.y) > .3f)
+        {
+            //if they are the player at the time of switching then switch to the cat
+            if (cf.target == transform) cf.target = cat.transform;
+            //if they are the cat at the time of switching switch to the player
+            else cf.target = transform;
+        }
+    }
 }
