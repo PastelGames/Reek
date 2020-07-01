34f1e8f25791b35cb9318fdb51b278c312d94f3b
timmarr98 <tmarr006@fiu.edu>
2020-06-30 21:16:17 -0400

    Added pressure plates

---
+++ Assets/Scripts/PressurePlates.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlates : MonoBehaviour
+{
+    public bool doorOpen;
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(!doorOpen)
+        {
+            doorOpen = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PressurePuzzle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePuzzle : MonoBehaviour
+{
+    public bool doorOpen1;
+    public GameObject pressure1;
+    public GameObject pressure2;
+    public GameObject door;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pressure1.GetComponent<PressurePlates>().doorOpen && pressure2.GetComponent<PressurePlates>().doorOpen &&  doorOpen1 == false)
+        {
+            doorOpen1 = true;
+            door.transform.position += new Vector3(0,4,0);
+        }
+    }
+}
