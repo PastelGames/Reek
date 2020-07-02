66b7303380602aa28aab47bcb596652e76834ccb
El'Christopher Tucker <elchristopher@outlook.com>
2020-07-02 12:45:17 -0400

    boss doesn't move until the boss fight is triggered

--- Assets/StartBossFight.cs
+++ Assets/StartBossFight.cs
@@ -8,6 +8,7 @@ public class StartBossFight : MonoBehaviour
     public GameObject door;
     public GameObject mainCamera;
     public GameObject bossCamera;
+    public GameObject boss;
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +26,17 @@ public class StartBossFight : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            //block the player from returning
             door.SetActive(true);
 
+            //swap out the cameras
             mainCamera.SetActive(false);
             mainCamera.tag = "Untagged";
             bossCamera.SetActive(true);
             bossCamera.tag = "MainCamera";
+
+            //start the bosses movement
+            boss.GetComponent<Animator>().SetTrigger("Boss Fight Started");
         }
     }
 }
